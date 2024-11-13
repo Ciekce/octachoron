80f3411ea69809d29fcf5a96befb8a48d7983cc5
Ciekce <kantaja98@gmail.com>
2024-11-13 19:01:40 +0000

    add license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
+/*
+ * Octachoron, a Pijersi engine
+ * Copyright (C) 2024 Ciekce
+ *
+ * Octachoron is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Octachoron is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Octachoron. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "types.h"
 
 #include <iostream>
